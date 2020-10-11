6418f6d76ff5477ab527f903ea19045d740bf97e
iGuoji <asgeg@qq.com>
2020-10-11 15:53:58 +0800

    调整

--- tests/app/Open/Brand.php
+++ tests/app/Open/Brand.php
@@ -18,11 +18,18 @@ class Brand
     public function save($req, $res)
     {
         return [
+            Db::beginTransaction(),
+            Db::execute('INSERT INTO `brand` VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [null, 0, '标题', 'BiaoTi', '描述', 'MiaoSu', 'icon', date('Y-m-d H:i:s')]),
+            Db::beginTransaction(),
+            Db::execute('INSERT INTO `brand` VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [null, 0, '标题2', 'BiaoTi', '描述', 'MiaoSu', 'icon', date('Y-m-d H:i:s')]),
+            Db::rollBack(),
+            Db::execute('INSERT INTO `brand` VALUES(?, ?, ?, ?, ?, ?, ?, ?)', [null, 0, '标题3', 'BiaoTi', '描述', 'MiaoSu', 'icon', date('Y-m-d H:i:s')]),
+            Db::commit(),
             Db::query('SELECT * FROM `brand` WHERE `id` = ?', [1]),
             Db::first('SELECT * FROM `brand` WHERE `id` = ?', [2]),
-            Db::number('SELECT COUNT(*) FROM `brand`'),
-            Db::number('SELECT SUM(`id`) FROM `brand`'),
-            Db::number('SELECT SUM(`id`) AS `count` FROM `brand`'),
+            Db::value('SELECT COUNT(*) FROM `brand`'),
+            Db::value('SELECT SUM(`id`) FROM `brand`'),
+            Db::value('SELECT SUM(`id`) AS `count` FROM `brand`'),
         ];
     }
 
